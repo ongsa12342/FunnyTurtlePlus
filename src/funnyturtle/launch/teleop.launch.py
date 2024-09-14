18b0b2ad4f525f1dce532495a8207c9dc547b5d3
Papinwit Rattanasiri <61608951+ongsa12342@users.noreply.github.com>
2024-09-14 20:54:41 +0700

    Update teleop.launch.py

--- src/funnyturtle/launch/teleop.launch.py
+++ src/funnyturtle/launch/teleop.launch.py
@@ -3,34 +3,36 @@ from launch_ros.actions import Node
 from launch.actions import ExecuteProcess
 
 def generate_launch_description():
-    name = 'turtle_teleop'
+    namespace = 'teleop'
+    turtlename = 'turtle_teleop'
     return LaunchDescription([
         # Launch turtlesim_plus_node.py with namespace 'turtle1'
         Node(
             package='turtlesim_plus',
             executable='turtlesim_plus_node.py',
-            namespace='teleop',
+            namespace=namespace,
             name='turtlesim_plus_node',
-            parameters=[{'turtle_name': 'turtle_teleop'}]
+            parameters=[{'turtle_name': turtlename}]
         ),
         
         # Launch teleop_controller_node.py
         Node(
             package='funnyturtle',
             executable='teleop_controller_node.py',
-            namespace='teleop',
+            namespace=namespace,
             name='teleop_controller_node',
-            parameters=[{'turtle_name': 'turtle_teleop'}]
+            parameters=[{'turtle_name': turtlename}]
         ),
         
         # Launch teleop_scheduler_node.py
         Node(
             package='funnyturtle',
             executable='teleop_scheduler_node.py',
-            namespace='teleop',
+            namespace=namespace,
             name='teleop_scheduler_node',
-            parameters=[{'turtle_name': 'turtle_teleop'}]
+            parameters=[{'turtle_name': turtlename}]
         ),
+        
         ExecuteProcess(
             cmd=['gnome-terminal', '--', 'ros2', 'run', 'funnyturtle', 'teleop_key_node.py',
                  '--ros-args', '--remap', '__ns:=/teleop', '-p', 'turtle_name:=turtle_teleop'],
@@ -43,7 +45,7 @@ def generate_launch_description():
         ),
 
         ExecuteProcess(
-                cmd=[f"ros2 service call /teleop/spawn_turtle turtlesim/srv/Spawn \"{{x: -1.0, y: -1.0, theta: 0.0, name: 'turtle_teleop'}}\""],
+                cmd=[f"ros2 service call /teleop/spawn_turtle turtlesim/srv/Spawn \"{{x: -1.0, y: -1.0, theta: 0.0, name: {turtlename}}}\""],
                 shell=True
             ),
     ])
